e2ada95c9a365aa4fec5c3d897ade43ffac7167e
Junho Yeo <hanaro0704@gmail.com>
2021-02-17 01:48:49 +0900

    🌸 feat: style profile elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function App({ Component, pageProps }) {
       <GlobalStyle />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap"
         rel="stylesheet"
       />
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,38 +8,47 @@ import ServiceWrapper from '../components/ServiceWrapper';
 const Home = () => {
   return (
     <ServiceWrapper>
-      <Avatar src={profile.photo_url} />
-      <Name>{profile.name}</Name>
-      <Username>{`@${profile.username}`}</Username>
-      <FollowRow>
-        <Followers>
-          {profile.num_followers}
-          <FollowPrefix>followers</FollowPrefix>
-        </Followers>
-        <Following>
-          {profile.num_following}
-          <FollowPrefix>following</FollowPrefix>
-        </Following>
-      </FollowRow>
-      <Bio>{profile.bio}</Bio>
-      <SocialRow>
-        <Instagram>{profile.instagram}</Instagram>
-        <Twitter>{profile.twitter}</Twitter>
-      </SocialRow>
-      <NorminationContainer>
-        <NorminationProfile src={profile.invited_by_user_profile.photo_url} />
-        <NorminationInformation>
-          <JoinedAt>{`Joined ${profile.time_created}`}</JoinedAt>
-          <NorminationText>
-            {`Norminated by ${profile.invited_by_user_profile.name}`}
-          </NorminationText>
-        </NorminationInformation>
-      </NorminationContainer>
+      <Wrapper>
+        <Avatar src={profile.photo_url} />
+        <Name>{profile.name}</Name>
+        <Username>{`@${profile.username}`}</Username>
+        <FollowRow>
+          <Followers>
+            {profile.num_followers}
+            <FollowPrefix>followers</FollowPrefix>
+          </Followers>
+          <Following>
+            {profile.num_following}
+            <FollowPrefix>following</FollowPrefix>
+          </Following>
+        </FollowRow>
+        <Bio>{profile.bio}</Bio>
+        <SocialRow>
+          <Instagram>{profile.instagram}</Instagram>
+          <Twitter>{profile.twitter}</Twitter>
+        </SocialRow>
+        <NorminationContainer>
+          <NorminationProfile src={profile.invited_by_user_profile.photo_url} />
+          <NorminationInformation>
+            <JoinedAt>{`Joined ${profile.time_created}`}</JoinedAt>
+            <NorminationText>
+              Norminated by{' '}
+              <Norminator>{profile.invited_by_user_profile.name}</Norminator>
+            </NorminationText>
+          </NorminationInformation>
+        </NorminationContainer>
+      </Wrapper>
     </ServiceWrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 52px 16px;
+`;
 
 const Avatar = styled.img`
   width: 80px;
@@ -47,19 +56,44 @@ const Avatar = styled.img`
   border-radius: 36px;
 `;
 
-const Name = styled.h1``;
+const Name = styled.h1`
+  margin: 0;
+  margin-top: 20px;
+  font-size: 1.2rem;
+`;
 
-const Username = styled.h2``;
+const Username = styled.h2`
+  margin-top: 15px;
+  font-size: 0.95rem;
+  font-weight: normal;
+`;
 
-const Bio = styled.p``;
+const FollowRow = styled.div`
+  margin-top: 27px;
+`;
 
-const FollowRow = styled.div``;
+const Followers = styled.span`
+  font-size: 1.3rem;
+  font-weight: 600;
+`;
 
-const Followers = styled.span``;
+const Following = styled(Followers)`
+  margin-left: 36px;
+`;
 
-const Following = styled(Followers)``;
+const FollowPrefix = styled.span`
+  font-size: 1rem;
+  margin-left: 4px;
+  font-weight: 300;
+`;
 
-const FollowPrefix = styled.span``;
+const Bio = styled.p`
+  margin-top: 34px;
+  white-space: pre-line;
+  font-size: 1.02rem;
+  font-weight: 300;
+  line-height: 1.45;
+`;
 
 const SocialRow = styled.div``;
 
@@ -67,7 +101,10 @@ const Instagram = styled.span``;
 
 const Twitter = styled(Instagram)``;
 
-const NorminationContainer = styled.div``;
+const NorminationContainer = styled.div`
+  margin-top: 45px;
+  display: flex;
+`;
 
 const NorminationProfile = styled.img`
   width: 36px;
@@ -75,8 +112,22 @@ const NorminationProfile = styled.img`
   border-radius: 16px;
 `;
 
-const NorminationInformation = styled.div``;
+const NorminationInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 12px;
+`;
 
-const JoinedAt = styled.span``;
+const JoinedAt = styled.span`
+  font-size: 0.92rem;
+  font-weight: 300;
+`;
 
-const NorminationText = styled.span``;
+const NorminationText = styled(JoinedAt)`
+  margin-top: 4px;
+`;
+
+const Norminator = styled.strong`
+  font-weight: 600;
+`;
