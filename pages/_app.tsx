cc4adb808a97cb9b68f73be2b60f7168a5a1eeb9
Junho Yeo <hanaro0704@gmail.com>
2021-02-20 16:33:36 +0900

    🚀 fix: render meta tags inside head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 import GlobalStyle from '../components/GlobalStyle';
@@ -5,19 +6,22 @@ import GlobalStyle from '../components/GlobalStyle';
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Junho's Clubhouse</title>
+        <meta property="og:title" content="Junho's Clubhouse" />
+        <meta property="og:url" content="https://clubhouse.junho.io/" />
+        <meta
+          property="og:description"
+          content="Come over to Junho's Clubhouse"
+        />
+        <meta property="og:image" content="/og-image.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyle />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap"
-        rel="stylesheet"
-      />
-      <meta property="og:title" content="Junho's Clubhouse" />
-      <meta property="og:url" content="https://clubhouse.junho.io/" />
-      <meta
-        property="og:description"
-        content="Come over to Junho's Clubhouse"
-      />
-      <meta property="og:image" content="/og-image.png" />
       <Component {...pageProps} />
     </>
   );
